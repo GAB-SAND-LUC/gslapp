50e5a977edce0af7678285c5a4fa2b3b22c14c90
Sandrine Morard <sandrinemorard@gmail.com>
2014-08-20 18:12:31 +0200

    Footer + landing page + reset password + cofirmation email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,17 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: 'XXXXX ' }
+
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   address:              'smtp.gmail.com',
+  #   port:                 465,
+  #   domain:               'http://gslapp.herokuapp.com/',
+  #   user_name:            'toto',
+  #   password:             'MY SECURE PASSWORD',
+  #   authentication:       :plain,
+  #   ssl:                  true
+  # }
+
+  config.action_mailer.default_url_options = { host: 'http://gslapp.herokuapp.com/' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
