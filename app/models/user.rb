c0d51be1cbccf49c6ba932ce84291ac3a8f5a35e
LucasPiessat <l.piessat@gmail.com>
2014-08-21 17:18:59 +0200

    w i p

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
   has_many :flats, dependent: :destroy
-  has_many :identities, depent: :destroy
+  has_many :identities, dependent: :destroy
 end
