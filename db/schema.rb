29bed5071a6ffd3d6785b31a9a0d0d0fbf60b065
LucasPiessat <l.piessat@gmail.com>
2014-08-22 15:04:15 +0200

    This is the end my only friend the end

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140821155640) do
+ActiveRecord::Schema.define(version: 20140822115950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,16 +89,16 @@ ActiveRecord::Schema.define(version: 20140821155640) do
     t.string   "phone"
     t.string   "address"
     t.string   "city"
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
     t.string   "provider"
     t.string   "uid"
     t.string   "picture"
     t.string   "name"
     t.string   "token"
     t.datetime "token_expiry"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
